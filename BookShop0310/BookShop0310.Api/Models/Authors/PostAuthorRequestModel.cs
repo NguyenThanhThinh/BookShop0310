6ba46e0e2cf1ed62360a7cd0458edfdcaa56c819
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-03 15:30:14 +0700

    add model author for web

---
+++ BookShop0310/BookShop0310.Api/Models/Authors/PostAuthorRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BookShop0310.Data;
+
+namespace BookShop0310.Api.Models.Authors
+{
+    using static DataConstants;
+
+    public class PostAuthorRequestModel
+    {
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string LastName { get; set; }
+    }
+}

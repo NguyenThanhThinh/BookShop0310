fdeabd483f2d4bb18eec95ee97ce089f8c080bec
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-03 09:59:22 +0700

    add Extensions

---
+++ BookShop0310/BookShop0310.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BookShop0310.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> enumerable)
+            => new HashSet<T>(enumerable);
+    }
+}

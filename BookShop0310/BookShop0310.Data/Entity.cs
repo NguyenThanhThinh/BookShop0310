0fbd187b6dbcd641811ef48266f7c5a995fa1182
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-03 09:12:43 +0700

    add base entity

---
+++ BookShop0310/BookShop0310.Data/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop0310.Data
+{
+    public class Entity<TId> : IEntity<TId>
+    {
+        public TId Id { get; set; }
+    }
+}
---
+++ BookShop0310/BookShop0310.Data/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop0310.Data
+{
+    public interface IEntity<TId>
+    {
+        TId Id { get; set; }
+    }
+}

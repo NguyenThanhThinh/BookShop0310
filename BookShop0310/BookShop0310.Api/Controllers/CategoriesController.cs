b5b9a87c9a4c6b6276cb7ce7d604972366a450d6
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-04 08:02:57 +0700

    update categories

--- BookShop0310/BookShop0310.Api/Controllers/CategoriesController.cs
+++ BookShop0310/BookShop0310.Api/Controllers/CategoriesController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using BookShop0310.Api.Infrastructure.Extensions;
 using BookShop0310.Api.Infrastructure.Filters;
 using BookShop0310.Api.Models.Categories;
 using BookShop0310.Services;
@@ -9,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop0310.Api.Controllers
 {
-    using BookShop0310.Api.Infrastructure.Extensions;
+    using static ApiConstants;
 
     public class CategoriesController : BaseController
     {
@@ -28,18 +26,45 @@ namespace BookShop0310.Api.Controllers
             return this.OkOrNotFound(result);
         }
 
+        [HttpGet(WithId)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var getCategoryById = await categoryService.GetByIdAsync(id);
+
+            return this.OkOrNotFound(getCategoryById);
+        }
+
         [HttpPost]
         [ValidateModelState]
         public async Task<IActionResult> Create([FromBody] CreateCategoryRequestModel model)
         {
-            var idResult = await this.categoryService.Create(model.Name);
+            var idResult = await categoryService.Create(model.Name);
 
-            if (idResult == 0)
-            {
-                return this.BadRequest($@"Category with Name: ""{model.Name}"" already exists.");
-            }
+            if (idResult == 0) return BadRequest($@"Category with Name: ""{model.Name}"" already exists.");
 
-            return this.Ok(idResult);
+            return Ok(idResult);
+        }
+
+        [HttpPut(WithId)]
+        [ValidateModelState]
+        public async Task<IActionResult> Edit(int id, [FromBody] EditCategoryNameRequestModel model)
+        {
+            var success = await categoryService.EditNameByIdAsync(id, model.Name);
+
+            if (!success)
+                return BadRequest($@"Category with Id {id} does not exist or Name: ""{model.Name}"" already exists.");
+
+            return Ok(await GetById(id));
+        }
+
+        [HttpDelete(WithId)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var success = await categoryService.Delete(id);
+
+            if (!success) return BadRequest($"Category with Id {id} does not exist");
+
+            return Ok("Deleted");
         }
     }
 }
